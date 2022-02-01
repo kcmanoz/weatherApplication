253ed220a0c8de96f6a71bdfafa71ce6254ff997
Manoj KC <manoj.kc@tuni.fi>
2022-02-01 16:58:32 +0200

    Dispaly weather info

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const enterButton = document.querySelector('.enter')
 const inputValue = document.querySelector('.inputValue')
-const name = document.querySelector('.cityName')
-const temo = document.querySelector('.temp')
+const cityName = document.querySelector('.cityName')
+const temp = document.querySelector('.temp')
 const desc = document.querySelector('.desc')
-
-//const apiKey = "f580de753052bf1267e78e268fb51e63";
 
 enterButton.addEventListener('click', function(){ 
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue.value}&appid=f580de753052bf1267e78e268fb51e63`)
 .then(response => response.json())
-.then(data  => console.log(data))
-
+.then(data  => {
+    const nameData = data['name'];
+    const tempData = data['main']['temp'];
+    const descData = data['weather'][0]['description'];
+    
+    cityName.innerHTML = nameData;
+    temp.innerHTML = tempData;
+    desc.innerHTML = descData;
+})
 .catch(err => alert("Something went wrong!"))
 });
+
