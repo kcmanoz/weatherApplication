2c008860fec36822e9448b6c81382d8377879ad2
Manoj KC <manoj.kc@tuni.fi>
2022-02-01 18:05:21 +0200

    Display modification

--- app.js
+++ app.js
@@ -5,15 +5,16 @@ const temp = document.querySelector('.temp')
 const desc = document.querySelector('.desc')
 
 enterButton.addEventListener('click', function(){ 
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue.value}&appid=f580de753052bf1267e78e268fb51e63`)
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue.value}&units=metric&appid=f580de753052bf1267e78e268fb51e63`)
 .then(response => response.json())
 .then(data  => {
     const nameData = data['name'];
-    const tempData = data['main']['temp'];
+    const tempData = Math.round(data['main']['temp']);
     const descData = data['weather'][0]['description'];
-    
+    //set DOM elements from the API
+    //using the hexadecimal code \u2103 to include degree celcius symbol
     cityName.innerHTML = nameData;
-    temp.innerHTML = tempData;
+    temp.innerHTML = tempData + "\u2103";
     desc.innerHTML = descData;
 })
 .catch(err => alert("Something went wrong!"))
