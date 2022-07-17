27b4ff719a50cb604de6d36add633850bbc5910d
Manoj KC <manoj.kc@tuni.fi>
2022-07-17 21:56:50 +0300

    minor changen

--- app.js
+++ app.js
@@ -2,27 +2,30 @@ const container = document.querySelector(".container")
 const enterButton = document.querySelector(".enter")
 const inputValue = document.querySelector(".inputValue")
 const cityName = document.querySelector(".cityName")
-const temp = document.querySelector(".temp")
+const temperature = document.querySelector(".temperature")
+const humid = document.querySelector(".humid")
 const desc = document.querySelector(".desc")
 const backArrow = document.querySelector(".material-icons-outlined")
 
 enterButton.addEventListener('click', function(){ 
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue.value}&units=metric&appid=f580de753052bf1267e78e268fb51e63`)
 .then(response => response.json())
-.then(data  => {
-    const nameData = data['name'];
-    const tempData = Math.round(data['main']['temp']);
-    const descData = data['weather'][0]['description'];
+.then(data  => weatherInfo(data))
+.catch(err => alert("Something went wrong!"))
+});
+
+function weatherInfo(data) {
+    const nameData = data.name;
+    const {temp, humidity} = data.main
+    const {description, id} = data.weather[0];
     //set DOM elements from the API
     //using the hexadecimal code \u2103 to include degree celsius symbol
     cityName.innerHTML = nameData;
-    temp.innerHTML = tempData + "\u2103";
-    desc.innerHTML = descData;
-
+    temperature.innerHTML = Math.floor(temp) + "\u2103";
+    humid.innerHTML = humidity + "%" ;
+    desc.innerHTML = description;
     container.classList.add("active");
-})
-.catch(err => alert("Something went wrong!"))
-});
+}
 
 backArrow.addEventListener("click", () =>{
     container.classList.remove("active");
