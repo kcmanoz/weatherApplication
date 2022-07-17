068822e134722affdd07682739266670a232f94f
Manoj KC <manoj.kc@tuni.fi>
2022-07-18 00:13:57 +0300

    minor changen

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const container = document.querySelector(".container")
+const errorText = document.querySelector(".error-text")
 const enterButton = document.querySelector(".enter")
 const inputValue = document.querySelector(".inputValue")
 const cityName = document.querySelector(".cityName")
@@ -15,16 +16,20 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue.value}&uni
 });
 
 function weatherInfo(data) {
-    const nameData = data.name;
-    const {temp, humidity} = data.main
-    const {description, id} = data.weather[0];
-    //set DOM elements from the API
-    //using the hexadecimal code \u2103 to include degree celsius symbol
-    cityName.innerHTML = nameData;
-    temperature.innerHTML = Math.floor(temp) + "\u2103";
-    humid.innerHTML = humidity + "%" ;
-    desc.innerHTML = description;
-    container.classList.add("active");
+    if(data.cod == "404"){
+        errorText.innerHTML = `${inputValue.value} is not a correct city name`;
+    }else{
+        const nameData = data.name;
+        const {temp, humidity} = data.main
+        const {description, id} = data.weather[0];
+        //set DOM elements from the API
+        //using the hexadecimal code \u2103 to include degree celsius symbol
+        cityName.innerHTML = nameData;
+        temperature.innerHTML = Math.floor(temp) + "\u2103";
+        humid.innerHTML = humidity + "%" ;
+        desc.innerHTML = description;
+        container.classList.add("active");
+    }
 }
 
 backArrow.addEventListener("click", () =>{
