cda76bc96ad29a0c0a23473dda5b720fd8662e5c
Manoj KC <manoj.kc@tuni.fi>
2023-07-20 08:30:50 +0300

    js file changes

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const inputBox = document.querySelector('.location-box');
 const enterBtn = document.querySelector('.enter-btn');
-const weatherIcons = document.querySelector('.weather-icon');
+const weatherIcon = document.querySelector('.weather-icon');
 const temperature = document.querySelector('.temperature');
 const description = document.querySelector('.description');
 const humidity = document.querySelector('#humidity');
 const windSpeed = document.querySelector('#wind-speed');
+
+const locationError = document.querySelector('.location-error');
+const weatherBody = document.querySelector('.weather-body');
 
 async function getWeatherData(city) {
     const api_key = "f580de753052bf1267e78e268fb51e63";
@@ -12,12 +15,40 @@ async function getWeatherData(city) {
 
     const weatherData = await fetch(`${url}`).then(response => response.json());
 
+    if (weatherData.cod === `404`) {
+        locationError.style.display = "flex";
+        weatherBody.style.display = "none";
+        console.log("error");
+        return;
+    }
+
+    locationError.style.display = "none";
+    weatherBody.style.display = "flex";
+
     temperature.innerHTML = `${Math.round(weatherData.main.temp)}°C`;
     description.innerHTML = `${weatherData.weather[0].description}`;
     humidity.innerHTML = `${weatherData.main.humidity}%`;
-    windSpeed.innerHTML = `${weatherData.wind.speed}`;
-}
+    windSpeed.innerHTML = `${weatherData.wind.speed} km/h`;
 
+    switch (weatherData.weather[0].main) {
+        case 'Clear':
+            weatherIcon.src = "/assets/clear.png";
+            break;
+        case 'Clouds':
+            weatherIcon.src = "/assets/clouds.png";
+            break;
+        case 'Rains':
+            weatherIcon.src = "/assets/rain.png";
+            break;
+        case 'Mist':
+            weatherIcon.src = "/assets/mist.png"
+            break;
+        case 'Snow':
+            weatherIcon.src = "/assets/snow.png";
+            break;
+    }
+
+}
 enterBtn.addEventListener('click', () => {
     getWeatherData(inputBox.value);
 });
